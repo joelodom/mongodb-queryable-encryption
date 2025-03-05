7a4c4cc492747d6fb1a93b4c3a057bfe7a131969
Joel Odom <joel.odom@mongodb.com>
2025-03-05 09:39:25 -0500

    Initial commit. Nothing to see here.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # mongodb-queryable-encryption
 A demonstration and sandbox for MongoDB Queryable Encryption.
+
+## First things first
+
+aoeu
---
+++ src/com/joelodom/QEDemonstration/QEDemonstration.java
@@ -0,0 +1,11 @@
+package com.joelodom.QEDemonstration;
+
+public class QEDemonstration {
+    public static final String WELCOME_MESSAGE = """
+Welcome to the Queryable Encryption demonstration by joel.odom@mongodb.com.
+""";
+
+    public static void main(String[] args) {
+        System.out.println(WELCOME_MESSAGE);
+    }
+}
