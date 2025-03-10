1d2b22bedb2dbca17d5246a24ad28882c393e1f6
Joel Odom <joel.odom@mongodb.com>
2025-03-10 14:09:51 -0400

    Range query is now working, but I need to add the parameters and comments about all that.

--- src/main/java/com/joelodom/Schemas.java
+++ src/main/java/com/joelodom/Schemas.java
@@ -8,24 +8,32 @@ import org.bson.BsonNull;
 import org.bson.BsonString;
 
 public class Schemas {
+
     /**
-     * The encrypted fields map specifices which fields in the collection
-     * should be encrypted, and which query type is allowed for the encrypted
-     * fields.
-     * 
-     * See https://www.mongodb.com/docs/manual/core/queryable-encryption/qe-create-encryption-schema/#std-label-qe-create-encryption-schema.
+     * The encrypted fields map specifices which fields in the collection should
+     * be encrypted, and which query type is allowed for the encrypted fields.
+     *
+     * See
+     * https://www.mongodb.com/docs/manual/core/queryable-encryption/qe-create-encryption-schema/#std-label-qe-create-encryption-schema.
      */
 
     public static final BsonDocument ENCRYPTED_FIELDS_MAP;
 
     static {
         ENCRYPTED_FIELDS_MAP = new BsonDocument().append("fields",
-            new BsonArray(Arrays.asList(
-                new BsonDocument()
-                        .append("keyId", new BsonNull())
-                        .append("path", new BsonString("ssn"))
-                        .append("bsonType", new BsonString("string"))
-                        .append("queries", new BsonDocument()
-                                .append("queryType", new BsonString("equality"))))));
+                new BsonArray(Arrays.asList(
+                        new BsonDocument()
+                                .append("keyId", new BsonNull())
+                                .append("path", new BsonString("ssn"))
+                                .append("bsonType", new BsonString("string"))
+                                .append("queries", new BsonDocument()
+                                        .append("queryType", new BsonString("equality"))),
+                        new BsonDocument()
+                                .append("keyId", new BsonNull())
+                                .append("path", new BsonString("age"))
+                                .append("bsonType", new BsonString("int"))
+                                .append("queries", new BsonDocument()
+                                        .append("queryType", new BsonString("range")))
+                )));
     }
 }
